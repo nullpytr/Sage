f7b251cc93cbf8e5c89fb9f20b32c60b3660e989
priyamkalra0 <priyamkalra0@gmail.com>
2026-01-14 12:18:49 +0300

    core/types: use sXX aliases for signed integers

--- include/Core/Types.hpp
+++ include/Core/Types.hpp
@@ -4,9 +4,9 @@
 using u8 = std::uint8_t;
 using u16 = std::uint16_t;
 using u32 = std::uint32_t;
-using i8 = std::int8_t;
-using i16 = std::int16_t;
-using i32 = std::int32_t;
+using s8 = std::int8_t;
+using s16 = std::int16_t;
+using s32 = std::int32_t;
 
 struct vec2f { float x, y; };
 struct vec3f { float x, y, z; };
