d34e8565a829826479b212c86da593dbf06b3216
priyamkalra0 <priyamkalra0@gmail.com>
2026-01-14 12:29:47 +0300

    core/types: added 64-bit integers

--- include/Core/Types.hpp
+++ include/Core/Types.hpp
@@ -4,9 +4,12 @@
 using u8 = std::uint8_t;
 using u16 = std::uint16_t;
 using u32 = std::uint32_t;
+using u64 = std::uint64_t;
+
 using s8 = std::int8_t;
 using s16 = std::int16_t;
 using s32 = std::int32_t;
+using s64 = std::int64_t;
 
 struct vec2f { float x, y; };
 struct vec3f { float x, y, z; };
