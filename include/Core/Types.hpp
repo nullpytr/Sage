eb013ca500605077bc0087b41a87e176036952e6
priyamkalra0 <priyamkalra0@gmail.com>
2026-05-29 19:06:27 +0530

    core/types: added STL type aliases and bigger wstrings

--- include/Core/Types.hpp
+++ include/Core/Types.hpp
@@ -1,6 +1,10 @@
 #pragma once
-#include <cstdint>
 
+/*
+ * These are the underlying types used in Nintendo's blobs;
+ * They can be used to directly read binary data from the files.
+ */
+#include <cstdint>
 using u8 = std::uint8_t;
 using u16 = std::uint16_t;
 using u32 = std::uint32_t;
@@ -20,3 +24,24 @@ using string64 = char[64];
 
 using wchar = char16_t;
 using wstring16 = wchar[16];
+using wstring32 = wchar[32];
+using wstring64 = wchar[64];
+
+using byte = unsigned char;
+
+/*
+ * STL types
+ * Sage automatically upgrades
+ * C style arrays into array-view (std::span)
+ * while C strings can be implicitly upgraded in user code
+ */
+#include <span>
+template<typename T>
+using array = std::span<T>;
+
+#include <string_view>
+using string = std::string_view;
+
+// template<typename T>
+// auto type_of(T const& t)
+// { return std::remove_reference_t<T>(); }
