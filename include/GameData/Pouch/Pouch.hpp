6eafddb01206403f6d670e573d5f9fb544283100
priyamkalra0 <priyamkalra0@gmail.com>
2026-06-05 19:34:21 +0530

    gamedata/pouch: include all new struct members
    all these new structs are auto generated from static/zelda-totk.hashes.csv; may not be fully accurate

--- include/GameData/Pouch/Pouch.hpp
+++ include/GameData/Pouch/Pouch.hpp
@@ -6,25 +6,40 @@
 
 #include "GameData/Pouch/Material/Material.hpp"
 #include "GameData/Pouch/KeyItem/KeyItem.hpp"
-#include "GameData/Pouch/Bow.hpp"
-#include "GameData/Pouch/Shield.hpp"
+#include "GameData/Pouch/Bow/Bow.hpp"
+#include "GameData/Pouch/Shield/Shield.hpp"
+#include "GameData/Pouch/Armor/Armor.hpp"
+#include "GameData/Pouch/Food/Food.hpp"
+#include "GameData/Pouch/Arrow/Arrow.hpp"
+#include "GameData/Pouch/SpecialParts/SpecialParts.hpp"
+#include "GameData/Pouch/SpecialPower/SpecialPower.hpp"
 #include "Weapon/Weapon.hpp"
 
 namespace GameData::Pouch {
     struct Pouch {
         Material::Data Material;
         KeyItem::Data KeyItem;
-        Bow Bow;
-        Shield Shield;
+        Bow::Data Bow;
+        Shield::Data Shield;
+        Armor::Data Armor;
+        Food::Data Food;
+        Arrow::Data Arrow;
+        SpecialParts::Data SpecialParts;
+        SpecialPower::Data SpecialPower;
         Weapon::Data Weapon;
 
         template <typename Sav>
         explicit Pouch(Sav& s)
-            : Material { s },
-              KeyItem  { s },
-              Bow      { s },
-              Shield   { s },
-              Weapon   { s }
+            : Material     { s },
+              KeyItem      { s },
+              Bow          { s },
+              Shield       { s },
+              Armor        { s },
+              Food         { s },
+              Arrow        { s },
+              SpecialParts { s },
+              SpecialPower { s },
+              Weapon       { s }
         {}
 
         struct Promise {};
