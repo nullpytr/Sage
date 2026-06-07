625aa5927e68e6b9a00ea82a2f1088b0d174ddb2
priyamkalra0 <priyamkalra0@gmail.com>
2026-06-08 05:03:30 +0530

    core/enum: split up views into their own headers

---
+++ include/Core/Enum/Scalar.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "Core/Types.hpp"
+#include "Core/Promise.hpp"
+
+namespace Enum
+{
+    template <typename EnumWrapperStruct>
+    struct Scalar : EnumWrapperStruct /* injects values enum into scalar's namespace */
+    {
+        /* type aliases */
+        using value_type = EnumWrapperStruct::Value;
+
+        /* View type for accessing single Enum entry;
+         * analogous to T& */
+
+        template <typename Sav>
+        explicit Scalar(Sav& s, Promise<value_type> const& promise)
+            : m_entry { s.get(promise) }
+        {}
+
+        /* Get entry */
+        value_type const& get() const { return m_entry; }
+        value_type& get() { return m_entry; }
+
+        value_type const& operator()() const { return get(); }
+        value_type& operator()()             { return get(); }
+
+        /* Assignment and conversion */
+        Scalar& operator=(value_type const& v) { m_entry = v; return *this; }
+        operator value_type() const { return get(); }
+
+        bool operator==(value_type const& v) const { return m_entry == v; }
+
+    private:
+        value_type& m_entry;
+    };
+}
