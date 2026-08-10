ae96b864035d374e67c12930aa22e978b166a572
priyamkalra0 <priyamkalra0@gmail.com>
2026-08-10 18:59:40 +0530

    core/enum: removed deprecated code

--- include/Core/Enum/Container.hpp
+++ include/Core/Enum/Container.hpp
@@ -3,8 +3,8 @@
 namespace Enum
 {
     template <typename E>
-    struct Container : E /* injects enum values into container's scope -- for ease of access */
+    struct Container : E /* parent struct used to inject enum values into this container's scope -- for ease of access */
     {
-        using enum_type = E::enum_type;
+        using enum_type = E::enum_type; /* parent struct also provides access to the actual enum type */
     };
 }
