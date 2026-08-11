7cff3158492e9d17ca03ae73d0fdea94b28628f1
priyamkalra0 <priyamkalra0@gmail.com>
2026-08-11 14:15:35 +0530

    core/types: added hash_t alias and Hashtable<> template
    1. hash_t alias gives the mmh type for this project's scope, i.e mmh32
    2. the Hashtable<> template will be used to encode hashes directly into the type system at compile time

--- include/Core/Types.hpp
+++ include/Core/Types.hpp
@@ -1,24 +1,6 @@
 #pragma once
 
-/* GameData types
- * These types are used in Sage's GameData model,
- * for constraining template substitution
- * in Sage's Sav::get<[S|M]> API
- */
-namespace Data
-{
-    struct Type {};
-    struct Structure : Type {};
-    struct Member : Type {};
-
-    /* A view specialization is a Data::Structure wrapper
-     * that can be injected with a save's data
-     * by constructing it with a Sav&
-     */
-    template <typename X>
-    struct View;
-}
-
+/* Primitive types: directly map to types used in Nintendo's blobs */
 #include <cstdint>
 using u8 = std::uint8_t;
 using u16 = std::uint16_t;
@@ -44,10 +26,11 @@ using wstring64 = wchar[64];
 
 using byte = unsigned char;
 
-struct rgb { u32& r; u32& g; u32& b; };
+/* MurmurHash3: Nintendo uses the 32 bit version in it's blobs */
+#include "External/MurmurHash3.hpp"
+using hash_t = mmh32;
 
-/*
- * STL types
+/* STL types
  * Sage automatically upgrades
  * C style arrays into array-view (std::span)
  * while C strings can be implicitly upgraded in user code
@@ -59,6 +42,35 @@ using array = std::span<T>;
 #include <string_view>
 using string = std::string_view;
 
-// template<typename T>
-// auto type_of(T const& t)
-// { return std::remove_reference_t<T>(); }
+/* GameData types
+ * These types are used in Sage's GameData model,
+ * for constraining template substitution
+ * in Sage's Sav::get<[S|M]> API
+ */
+namespace Data
+{
+    struct Type {};
+    struct Structure : Type {};
+
+    /* A view (specialization of this template)
+     * is like an instantiable 'copy' of a
+     * auto generated Data::Structure types;
+     * but injected with save data from a Sav&
+     */
+    template <typename S>
+    struct View;
+
+    struct Member : Type {};
+
+    /* Specializations of this template
+     * are simply used to store hashes for
+     * auto generated Data::Member types
+     */
+    template <typename M>
+    inline constexpr hash_t Hashtable = [] {
+        /* This static assert ensures that no other type
+         * can fall back to this non-specialised template (because this is not an impl)
+         * -- we throw a hard error for undefined specializations. */
+        static_assert(false, "no hashtable entry found");
+    };
+}
