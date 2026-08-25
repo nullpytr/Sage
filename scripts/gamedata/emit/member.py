365585d1940bf738bdeba6ea0351b83296f02bf8
priyamkalra0 <priyamkalra0@gmail.com>
2026-08-25 15:19:22 +0530

    scripts/gd/emit/member: added initial support for adapters

--- scripts/gamedata/emit/member.py
+++ scripts/gamedata/emit/member.py
@@ -8,8 +8,21 @@ class MemberEmitter():
         buffer: list[str] = []
         write = buffer.append
 
+        return_type = raw_type = member.typename
+
+        # TODO: bake this into python's type system instead of forcing it here
+        if isinstance(member, (types.Array, types.String)):
+            raw_type = f"adapter<{raw_type}>"
+
+        if isinstance(member, types.MemberPointer): raw_type += "*"
+        else: raw_type += "&"
+
+        if isinstance(member, types.Primitive):
+            return_type += "&"
+
         write(f"struct {member.name} : {member.basename}" " {") # def open
-        write(f"using value_type = ::{member.typename};")
+        write(f"using type = {return_type};")
+        write(f"using adapter = {raw_type};")
         write("};") # def close
 
         return delim.join(buffer)
