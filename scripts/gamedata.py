5a437a071efd219407f8f5137572daba3bf8d479
priyamkalra0 <priyamkalra0@gmail.com>
2026-08-25 15:16:34 +0530

    scripts/gd: target `data/gamedata-medium.csv`

--- scripts/gamedata.py
+++ scripts/gamedata.py
@@ -3,8 +3,9 @@ from pathlib import Path
 import json
 
 proj_root = Path(__file__).parents[1]
-gamedata_file = proj_root / "data" / "gamedata-basic.csv" # in
+gamedata_file = proj_root / "data" / "gamedata-medium.csv" # in
 header_include_dir = proj_root / "include" # out
+# header_file_path = proj_root / "include" / "GameData.hpp"
 
 gamedata = tree.Tree(name="GameData", path="GameData", children={})
 tree.parse_csv(open(gamedata_file, "r"), gamedata)
@@ -15,6 +16,7 @@ gamedata_dict = json.loads(
 print(json.dumps(gamedata_dict, indent=1))
 
 emitter = emit.structure.StructureEmitter
+# header_file_path.write_text(emitter.emit(gamedata))
 emitter.emit(gamedata, include_dir=header_include_dir)
 
 print("Project root:", proj_root)
