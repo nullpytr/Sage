db3e5aedbe3febd95b95820b382a064485c8121e
priyamkalra0 <priyamkalra0@gmail.com>
2026-08-27 17:01:22 +0530

    scripts/gd: clear target header directory before emit

--- scripts/gamedata.py
+++ scripts/gamedata.py
@@ -1,13 +1,15 @@
 from gamedata import emit, tree
 from pathlib import Path
+from shutil import rmtree
 import json
 
 proj_root = Path(__file__).parents[1]
 gamedata_file = proj_root / "data" / "gamedata-medium.csv" # in
+root_header_name = "GameData"
 header_include_dir = proj_root / "include" # out
-# header_file_path = proj_root / "include" / "GameData.hpp"
+# header_file_path = proj_root / "include" / f"{root_header_name}.hpp"
 
-gamedata = tree.Tree(name="GameData", path="GameData", children={})
+gamedata = tree.Tree(name=root_header_name, path=root_header_name, children={})
 tree.parse_csv(open(gamedata_file, "r"), gamedata)
 
 gamedata_dict = json.loads(
@@ -15,6 +17,9 @@ gamedata_dict = json.loads(
 )
 print(json.dumps(gamedata_dict, indent=1))
 
+header_dir = header_include_dir / root_header_name
+if header_dir.exists(): rmtree(header_dir)
+    
 emitter = emit.structure.StructureEmitter
 # header_file_path.write_text(emitter.emit(gamedata))
 emitter.emit(gamedata, include_dir=header_include_dir)
