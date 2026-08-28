e225ac75597bc45ea4b1cf255392294dfc1c1303
priyamkalra0 <priyamkalra0@gmail.com>
2026-08-28 18:24:34 +0530

    scripts/gd/cli: added some guardrails to resolve conflicting flags
    and moved around some code according to preference

--- scripts/gamedata.py
+++ scripts/gamedata.py
@@ -5,7 +5,7 @@ from contextlib import redirect_stdout
 from shutil import rmtree
 from pathlib import Path
 
-from gamedata import tree
+from gamedata import *
 
 PROJECT_ROOT = Path(__file__).parents[1] # scripts/..
 PRESETS_DIR = PROJECT_ROOT / "data" / "presets"
@@ -13,17 +13,9 @@ PRESET_CHOICES = tuple(sorted(p.stem for p in PRESETS_DIR.iterdir()))
 
 SINK_FD = open(os.devnull, "w")
 
-def as_dict(t: tree.Tree) -> dict:
-    s = repr(t).replace("'", '"')
-    return json.loads(s)
-
-def resolve_datafile(args: argparse.Namespace) -> Path:
-    if args.metadata: return args.metadata
-
-    preset_file = PRESETS_DIR / f"{args.preset}.txt"
-    assert preset_file.exists(), f"preset does not exist: {args.preset}"
-
-    return preset_file
+DEFAULT_PRESET_CHOICE = "medium"
+DEFAULT_OUTPUT_DIR = PROJECT_ROOT / "lib"
+DEFAULT_TOP_LEVEL_HEADER_NAME = "GameData"
 
 def build_parser() -> argparse.ArgumentParser:
     p = argparse.ArgumentParser(
@@ -35,9 +27,9 @@ def build_parser() -> argparse.ArgumentParser:
     src.add_argument(
         "--preset", "-p",
         choices=PRESET_CHOICES,
-        default="medium",
+        default=DEFAULT_PRESET_CHOICE,
         metavar="PRESET",
-        help=f"Metadata preset to use: ({' | '.join(PRESET_CHOICES)}) (default: medium)",
+        help=f"Metadata preset to use: ({' | '.join(PRESET_CHOICES)}) (default: {DEFAULT_PRESET_CHOICE})",
     )
     src.add_argument(
         "--metadata", "-m",
@@ -50,20 +42,23 @@ def build_parser() -> argparse.ArgumentParser:
     p.add_argument(
         "--out", "-o",
         type=Path,
-        default=PROJECT_ROOT / "lib",
+        default=DEFAULT_OUTPUT_DIR,
         metavar="DIR",
-        help="Output directory for generated headers (default: lib/)",
+        help=f"Output directory for generated headers (default: {DEFAULT_OUTPUT_DIR})",
     )
-    p.add_argument(
-        "--name", "-n",
-        default="GameData",
+
+    spec = p.add_mutually_exclusive_group()
+    spec.add_argument(
+        "--name", "-n", 
         metavar="NAME",
-        help="Aggregate header name (default: GameData)",
+        default=DEFAULT_TOP_LEVEL_HEADER_NAME,
+        help=f"Top-level header name (default: {DEFAULT_TOP_LEVEL_HEADER_NAME})",
     )
+
     p.add_argument(
         "--standalone", "-s",
         action="store_true",
-        help="Emit only single standalone header"
+        help="Emit only single standalone header",
     )
     p.add_argument(
         "--dry",
@@ -103,8 +98,9 @@ def run(args: argparse.Namespace) -> None:
         data_fp = resolve_datafile(args)
         assert data_fp.exists(), f"metadata file does not exist: {data_fp}"
 
+
     with redirect_stdout(verbose_fd):
-        gamedata = tree.Tree(name=args.name, path=args.name, children={})
+        gamedata = tree.Tree(args.name)
         if data_fp: tree.make_tree(data_fp.open("r"), gamedata)
 
     if args.tree and data_fp: return print(json.dumps(as_dict(gamedata), indent=2))
@@ -114,7 +110,7 @@ def run(args: argparse.Namespace) -> None:
     header_dir = out_dir / str(args.name)
     header_file = header_dir.with_suffix(".hpp")
 
-    should_clear_header_dir =  not (args.standalone or args.dirty) 
+    should_clear_header_dir = not (args.standalone or args.dirty) 
     if should_clear_header_dir and header_dir.exists():
         rmtree(header_dir)
         print("[cli/clear]", f"cleared {header_dir}")
@@ -126,13 +122,15 @@ def run(args: argparse.Namespace) -> None:
 
     if args.clear: return 
 
-    emit = tree.TreeEmitter.emit
-    with redirect_stdout(verbose_fd):
-        if args.standalone: emit(gamedata, header_fp=header_file)
-        else: emit(gamedata, include_dir=out_dir)
+    args.standalone = args.standalone or isinstance(gamedata, types.Member) # member always standalone
 
-    print(header_file)
+    emitter = tree.TreeEmitter
+    with redirect_stdout(verbose_fd):
+        if args.standalone: emitter.emit(gamedata, header_fp=header_file)
+        else: emitter.emit(gamedata, include_dir=out_dir)
+
     if not args.standalone: print(header_dir)
+    print(header_file)
 
 def main() -> None:
     try: run(build_parser().parse_args())
@@ -142,5 +140,17 @@ def main() -> None:
 
     sys.exit(0)
 
+def as_dict(t: types.GameDataType) -> dict:
+    s = repr(t).replace("'", '"')
+    return json.loads(s)
+
+def resolve_datafile(args: argparse.Namespace) -> Path:
+    if args.metadata: return args.metadata
+
+    preset_file = PRESETS_DIR / f"{args.preset}.txt"
+    assert preset_file.exists(), f"preset does not exist: {args.preset}"
+
+    return preset_file
+    
 if __name__ == "__main__":
     main()
