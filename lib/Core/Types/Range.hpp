c1817594cdb69d946cf9058fc77b0d82ef37f324
priyamkalra0 <priyamkalra0@gmail.com>
2026-08-31 21:57:04 +0530

    core/types/ranges: added `owning_range`
    This range copies an array of `T`s, so it stays alive even if the original array's lifetime expires.

--- lib/Core/Types/Range.hpp
+++ lib/Core/Types/Range.hpp
@@ -6,3 +6,6 @@
 /* Range */
 template <typename T, typename Fn>
 using range = std::ranges::transform_view<std::span<T>, Fn>;
+
+template <typename T, size_t N, typename Fn>
+using owning_range = std::ranges::transform_view<std::ranges::owning_view<std::array<T, N>>, Fn>;
