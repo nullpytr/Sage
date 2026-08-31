053c88f591d7f42ead56cdf59e948cd7192a0466
priyamkalra0 <priyamkalra0@gmail.com>
2026-08-31 22:04:15 +0530

    core/types/ranges: added `mapped_range<T, N>`
    mapped_range dereferences pointer in a ptr_array (map<T, N>) lazily on access

--- lib/Core/Types/Range.hpp
+++ lib/Core/Types/Range.hpp
@@ -9,3 +9,7 @@ using range = std::ranges::transform_view<std::span<T>, Fn>;
 
 template <typename T, size_t N, typename Fn>
 using owning_range = std::ranges::transform_view<std::ranges::owning_view<std::array<T, N>>, Fn>;
+
+/* mapped_range dereferences pointer in a pointer array (map<T, N>) lazily on access */
+inline auto deref = [](auto* p) -> auto& { return *p; };
+template <typename T, size_t N> using mapped_range = owning_range<T*, N, decltype(deref)>;
